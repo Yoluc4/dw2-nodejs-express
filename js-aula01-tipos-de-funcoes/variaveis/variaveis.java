4a865c704e3b57ec2503c93ea31556d8102be79b
DSM2 <fatec-dsm2@academico.intra>
2026-02-11 20:58:03 -0300

    'Aula 01 - Varíaveis

---
+++ js-aula01-tipos-de-funcoes/variaveis/variaveis.java
@@ -0,0 +1,23 @@
+//Comentario de linha
+
+/* Cometário 
+ de bloco */
+//VARIAVEIS PODEM SER DECLARADAS DE 3 FORMAS:
+//VAR, LET, CONST
+// VAR: No geral evite o seu uso, pode não ser muito seguro.
+// LET: Utilize quando for necessário reatribuir o valor da varíavel.
+// CONST: Utilize quando NÃO precisar reatribuir o valor da variíavel.
+
+var cidade ="Registro";
+var cidade = "Sete Barras";
+document.write(cidade);
+
+let nome = "Luca";
+nome = "Lucas";
+document.write("<br>");
+document.write(nome);
+
+const pais = "Brasil";
+pais = "Argentina"; // O valor de uma constante não pode ser alterado.
+document.write("<br>");
+document.write(pais);
